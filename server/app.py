0ec5b63bc9e286af29ed8da327851a00139e7f1f
evmn <38780274+evmn@users.noreply.github.com>
2023-04-27 10:11:41 +0800

    local server build with flask at the help of GPT

---
+++ server/app.py
@@ -0,0 +1,66 @@
+from flask import Flask, render_template
+import random
+import sqlite3
+
+def create_connection():
+    database_file = "../db/Encyclopedia_of_Math-20230425.db"
+    connection = None
+    try:
+        connection = sqlite3.connect(database_file)
+        print("Connection to SQLite DB successful")
+    except sqlite3.Error as e:
+        print(f"The error '{e}' occurred")
+
+    return connection.cursor()
+
+#database_file = "Encyclopedia_of_Math-20230425.db"
+#connection = create_connection(database_file)
+
+
+app = Flask(__name__)
+
+# Connect to your database here
+@app.route('/')
+def index():
+
+    return render_template('index.html')
+
+@app.route('/toc')
+def table_of_contents():
+    # Fetch data from the 'entry' table
+    cursor = create_connection()
+    cursor.execute("SELECT * FROM entry  where redirect=0")
+    entries = cursor.fetchall()
+
+    return render_template('table_of_contents.html', entries=entries)
+
+@app.route('/entry/<int:entry_id>')
+def show_entry(entry_id):
+    # Fetch data from the 'entry' and 'latex' tables
+    cursor = create_connection()
+    cursor.execute("SELECT * FROM entry WHERE id=? and redirect=0", (entry_id,))
+    entry = cursor.fetchone()
+    cursor.execute("SELECT * FROM latex WHERE eid=?", (entry_id,))
+    latex = cursor.fetchone()
+
+    return render_template('entry.html', entry=entry, latex=latex)
+
+@app.route('/search')
+def search():
+    # Implement your search functionality here
+    return "Search Results Page"
+
+@app.route('/random')
+def random_page():
+    # Get a random page from the list
+    cursor = create_connection()
+    cursor.execute("SELECT id FROM entry WHERE redirect=0 order by random() limit 1")
+    entry_id = cursor.fetchone()
+    
+    # Return the page as a template
+    #return render_template(f'{page}.html')
+    return show_entry(entry_id[0])
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
